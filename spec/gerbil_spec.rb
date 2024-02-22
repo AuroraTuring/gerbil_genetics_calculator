b168e5fafe89822519052f2f919aa8e8af813d1f
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-22 13:03:02 -0700

    Change capitals to lowercase

--- spec/gerbil_spec.rb
+++ spec/gerbil_spec.rb
@@ -2,11 +2,11 @@ require "./spec/spec_helper"
 
 RSpec.describe Gerbil do
   before(:each) do
-    @gerbil1 = Gerbil.new("Agouti", "white belly", :male)
-    @gerbil2 = Gerbil.new("Lilac", "spot", :male)
-    @gerbil3 = Gerbil.new("Yellow Fox", "pied", :female)
-    @gerbil4 = Gerbil.new("Grey Agouti", "self", :female)
-    @gerbil5 = Gerbil.new("Black", "mottled", :female)
+    @gerbil1 = Gerbil.new("agouti", "white belly", :male)
+    @gerbil2 = Gerbil.new("lilac", "spot", :male)
+    @gerbil3 = Gerbil.new("yellow fox", "pied", :female)
+    @gerbil4 = Gerbil.new("grey agouti", "white belly", :male)
+    @gerbil5 = Gerbil.new("black", "mottled", :female)
   end
 
   describe "#initialize" do
@@ -15,13 +15,13 @@ RSpec.describe Gerbil do
     end
 
     it "has correct attributes" do
-      expect(@gerbil1.color).to eq("Agouti")
+      expect(@gerbil1.color).to eq("agouti")
       expect(@gerbil1.pattern).to eq("white belly")
       expect(@gerbil1.gender).to eq(:male)
     end
 
     it "has incorrect attributes" do
-      expect(@gerbil1.color).to eq("Rainbow")
+      expect(@gerbil1.color).to eq("rainbow")
       expect(@gerbil1.pattern).to eq("blurry")
       expect(@gerbil1.gender).to eq(:female)
     end
