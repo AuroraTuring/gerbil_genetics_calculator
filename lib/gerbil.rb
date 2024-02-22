eecdc138cb720d07118238d7a2ee5349326ab5d8
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-21 18:42:31 -0700

    Initialize with attributes

--- lib/gerbil.rb
+++ lib/gerbil.rb
@@ -1,6 +1,11 @@
 class Gerbil
+  attr_reader :color, :pattern, :gender
 
-
+  def initialize(color, pattern, gender)
+    @color = color
+    @pattern = pattern
+    @gender = gender
+  end
 
 
 
--- spec/gerbil_spec.rb
+++ spec/gerbil_spec.rb
@@ -2,16 +2,28 @@ require "./spec/spec_helper"
 
 RSpec.describe Gerbil do
   before(:each) do
-    gerbil1 = Gerbil.new("Agouti", "white belly", :male)
-    gerbil2 = Gerbil.new("Lilac", "spot", :male)
-    gerbil3 = Gerbil.new("Yellow Fox", "pied", :female)
-    gerbil4 = Gerbil.new("Grey Agouti", "self", :female)
-    gerbil5 = Gerbil.new("Black", "mottled", :female)
+    @gerbil1 = Gerbil.new("Agouti", "white belly", :male)
+    @gerbil2 = Gerbil.new("Lilac", "spot", :male)
+    @gerbil3 = Gerbil.new("Yellow Fox", "pied", :female)
+    @gerbil4 = Gerbil.new("Grey Agouti", "self", :female)
+    @gerbil5 = Gerbil.new("Black", "mottled", :female)
   end
 
   describe "#initialize" do
     it "can initialize with attributes" do
-      expect(gerbil1).to be_a(Gerbil)
+      expect(@gerbil1).to be_a(Gerbil)
+    end
+
+    it "has correct attributes" do
+      expect(@gerbil1.color).to eq("Agouti")
+      expect(@gerbil1.pattern).to eq("white belly")
+      expect(@gerbil1.gender).to eq(:male)
+    end
+
+    it "has incorrect attributes" do
+      expect(@gerbil1.color).to eq("Rainbow")
+      expect(@gerbil1.pattern).to eq("blurry")
+      expect(@gerbil1.gender).to eq(:female)
     end
   end
 end
