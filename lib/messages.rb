27d112421f1a2fa7e4e1870dc9955fe367313ba4
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-23 10:22:47 -0700

    Add a welcome message

---
+++ lib/messages.rb
@@ -0,0 +1,19 @@
+class Messages
+
+
+  def self.welcome
+    "Welcome! Please input the mother's color."
+  end
+
+  # def self.invalid
+    # if invalid input
+    # p "Please type a corret gerbil color."
+  # end
+
+
+
+# if valid, "Thank you! Now input the father's color/genetics."
+
+
+
+end
