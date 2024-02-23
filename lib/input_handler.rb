6584a67e3319e5e614b421d0b134a20b7483cc20
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-23 10:23:19 -0700

    Begin designing a #launch

--- lib/input_handler.rb
+++ lib/input_handler.rb
@@ -2,6 +2,27 @@ class InputHandler
 
 
 
+  # def receive_input
+    # wait for user input
+    # Class checks if input is valid
+  # end
+
+
+
+def launch
+  puts Messages.welcome
+  # receives mother's input
+  mothers_input = gets.chomp
+  # is mother's input valid
+
+  # check against color list
+  gerbil.valid_color?
+  # puts Messages.pattern
+  # receive father's input
+  fathers_input = gets.chomp
+  # puts Messages.possible_babies
+end
+
 
 
 end
