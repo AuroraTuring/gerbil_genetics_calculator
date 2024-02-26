ab2987fbf4481fca328b14b81aaff56a4132f77a
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-26 12:27:49 -0700

    Add tests for all greetings

--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -2,7 +2,7 @@ require "./spec/spec_helper"
 
 RSpec.describe Messages do
 
-  describe "#welcome" do
+  describe "#launch" do
     it "welcomes the user and asks for input" do
       expect(Messages.welcome).to eq("Welcome! Please input the mother's color.")
     end
@@ -10,8 +10,16 @@ RSpec.describe Messages do
     it "has the correct answer message when an incorrect color is provided" do
       expect(Messages.invalid).to eq("Please type a color that matches the following list: agouti, lilac, yellow_fox, grey_agouti, black")
     end
-  end
 
+    it "asks for father color after mother color" do
+      expect(Messages.father).to eq("Thank you! Now please input the father's color.")
+    end
+
+    it "populates a list of baby colors after parent colors have been inputted" do
+      expect(Messages.babies).to eq("Here is a list of all possible pup colors for these parents:")
+    end
+
+  end
 
 
 end
