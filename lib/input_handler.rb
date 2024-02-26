bb50989bdca018374ad38b14171d09ba7f484995
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-26 12:51:36 -0700

    Add a message for if an invalid color is inputted

--- lib/input_handler.rb
+++ lib/input_handler.rb
@@ -4,13 +4,17 @@ class InputHandler
   def launch
     puts Messages.welcome
     mothers_input = gets.chomp
-    #Class checks that color is valid
-    Gerbil.valid_color?(mothers_input)
+    if !Gerbil.valid_color?(mothers_input)
+      puts Messages.invalid
+      return false
+    end
 
     puts Messages.father
     fathers_input = gets.chomp
-    #Class checks that color is valid
-    Gerbil.valid_color?(fathers_input)
+    if !Gerbil.valid_color?(fathers_input)
+      puts Messages.invalid
+      return false
+    end
 
     puts Messages.babies
   end
