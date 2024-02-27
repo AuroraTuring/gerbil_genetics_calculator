545df25d49f0f2b722103d1b528ec8edcdd6a76d
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-26 17:34:16 -0700

    Add loop for putsing a message when color is invalid and moving on if valid

--- lib/input_handler.rb
+++ lib/input_handler.rb
@@ -1,21 +1,29 @@
 class InputHandler
 
-
   def launch
     puts Messages.welcome
-    mothers_input = gets.chomp
-    if !Gerbil.valid_color?(mothers_input)
-      puts Messages.invalid
-      return false
+    mothers_input = false
+    loop do
+      mothers_input = gets.chomp
+      if !Gerbil.valid_color?(mothers_input)
+        puts Messages.invalid
+      else
+        break
+      end
     end
+
+
 
     puts Messages.father
-    fathers_input = gets.chomp
-    if !Gerbil.valid_color?(fathers_input)
-      puts Messages.invalid
-      return false
+    fathers_input = false
+    loop do
+      fathers_input = gets.chomp
+      if !Gerbil.valid_color?(fathers_input)
+        puts Messages.invalid
+      else
+        break
+      end
     end
-
     puts Messages.babies
   end
 
