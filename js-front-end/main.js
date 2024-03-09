c9b696971d7f68e2c6d0bd3ca74af64c61591e46
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-03-08 19:00:23 -0700

    feat: Will add simple validation of text entry to prevent bugs.

--- js-front-end/main.js
+++ js-front-end/main.js
@@ -1,10 +1,37 @@
-const placeholderDatamodel = [];
-const submitButton1 = document.querySelector(".submit-button")
+var testContainer = {}
+const validCharacters = ['e','u','w','(',')','f','s','p','r']
+const submitButton = document.querySelector(".submit-button")
+const genotypeEntry = document.querySelector('.genotype-entry')
 
-submitButton1.addEventListener('click', function(event){
-    let element = event.target.parentElement;
-    Array.from(element.children).forEach(gene => {
-        placeholderDatamodel.push(gene.value)
-        gene.value=''     
+submitButton.addEventListener('click', function(event){
+    let toExportEntry = {
+        genes : []
+    };
+    let validSubmission = true
+    Array.from(genotypeEntry.children).forEach(gene => {
+        let isValidInput = validateInput(gene.value)
+        if (!isValidInput){
+            validSubmission = false
+        };
+        toExportEntry.genes.push(gene.value)
     });
+    console.log(validSubmission)
+    if (validSubmission){
+        console.log(toExportEntry.genes)
+    } else {
+        delete toExportEntry
+        return "error: invalid entry"
+    }
 });
+
+
+function validateInput(string){
+    let validInput = true
+    let toValidate = string.toLowerCase().split('')
+    toValidate.forEach(element => {
+        if(!validCharacters.includes(element)){
+            validInput = false
+        }
+    });
+    return validInput;
+}
