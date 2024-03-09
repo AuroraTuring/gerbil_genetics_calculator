8c61e1ff2556abdd8076a85e03da4cb711f6539a
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-03-08 17:42:38 -0700

    refactor: Will change to for each loop for simplicity.

--- js-front-end/main.js
+++ js-front-end/main.js
@@ -1,13 +1,10 @@
 const placeholderDatamodel = [];
-const submitButton = document.querySelector('.submit-button')
+const submitButton1 = document.querySelector(".submit-button")
 
-submitButton.addEventListener('click', function(event){
+submitButton1.addEventListener('click', function(event){
     let element = event.target.parentElement;
-    for (let i = 0; i < 6; i++) {
-        const gene = element.children[i].value
-        placeholderDatamodel.push(gene)
-        element.children[i].value = ''
-    }
-    console.log(placeholderDatamodel)
-
-})
+    Array.from(element.children).forEach(gene => {
+        placeholderDatamodel.push(gene.value)
+        gene.value=''     
+    });
+});
