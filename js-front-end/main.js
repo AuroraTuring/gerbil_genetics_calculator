ea23b8e28e32a7433a838c65f0f1adfe381d2016
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-03-09 15:22:33 -0700

    feat: Will finish the decision tree of whether to save user data to json file for back end parsing, the next step will be doing some basic styling and adjusting html to better standards before moving on to webpack to allow usage of the json file.

--- js-front-end/main.js
+++ js-front-end/main.js
@@ -1,15 +1,25 @@
+import { readFileSync, writeFileSync } from 'fs'
 const validCharacters = ['e','u','w','(',')','f','s','p','r']
 const submitButton = document.querySelector(".submit-button")
-const genotypeEntry = document.querySelector('.genotype-entry')
 const parent1 = document.getElementById('parent1')
 const parent2 = document.getElementById('parent2')
+const userInput = readFileSync('user_input.json');
+const jsonData = JSON.parse(userInput)
+document.addEventListener('DOMContentLoade', function(){
 
+});
 submitButton.addEventListener('click', function(){
     let toGenerate = Array.from(parent1.children);
     let entry1 = generateParent('m',toGenerate);
     toGenerate = Array.from(parent2.children)
     let entry2 = generateParent('f',toGenerate)
     console.log(entry1, entry2);
+    if (entry1 === "error" || entry2 === "error") {
+       //print error message
+    } else {
+        jsonData.parents.push
+        writeFileSync('user_input.json', JSON.stringify(jsonData))
+    }
 });
 
 function generateParent(parent, entry){
