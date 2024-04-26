53b79c3a4600ea90abf02b38dfffc59210dcaf2f
Aurora Sanderson <AuroraTuring@gmail.com>
2024-04-26 09:58:22 -0600

    Update notes

--- mentoring_notes.rb
+++ mentoring_notes.rb
@@ -4,6 +4,22 @@ mother_genetics = {agouti: 'aa', color: 'C*', dilute: 'D*', extension: 'E*', pin
 f_allele1, f_allele2 = father_genetics[:agouti].split
 m_allele1, m_allele2 = mother_genetics[:agouti].split
 
+# examples = {genetics => color}
+examples = {AaCcchmDdEePpUWuwd: :agouti,
+            AACcchmDdEePpUwuwd: :agouti,
+            AaCCDdEePpUwuwd: :agouti,
+            AaCcchmDDEePpUwuwd: :agouti,
+            AaCcchmDdEEPpWuwu: :agouti,
+            AaCcchmDdEePPUwuwd: :agouti,
+            AaCcchmDdEePpUwuwd: :agouti}
+
+letter_variations = []
+a_letters.each do |aletter|
+  c_letters.each do |cletter|
+    letter_variations << aletter + cletter
+  end
+end
+
 # possible combinations: Aa, aa
 # CC, Cc(chm), Cc(h), c(chm)c(chm), c(chm)c(h), c(h)c(h).
 # DD, Dd, dd
@@ -15,6 +31,17 @@ m_allele1, m_allele2 = mother_genetics[:agouti].split
 # dilute: DD, Dd, dd
 # UwUw, Uwuw(d), and uw(d)uw(d). Do not include the lowercase uwuw mutation.
 # EE, Ee, Ee(f), ee, ee(f), e(f)e(f)
+
+
+
+
+# aa C* dd ee pp uw[d]uw[d]
+
+# Possible combinations:
+
+
+# Combinations that matter:
+
 
 
 class GerbilCalculator
